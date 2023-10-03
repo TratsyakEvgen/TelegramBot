6b4aa1bdf73eadc5281d771ac0ab8127fba580d5
admin <tanchik776@gmail.com>
2023-10-03 21:30:18 +0300

    Initial commit

---
+++ src/test/java/com/tratsiak/telegarmbot/TelegarmBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tratsiak.telegarmbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegarmBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
